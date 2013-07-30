33819ea5e174564e5d57c1d982ac2147f0ffc15e
nolidais <nolidais@gmail.com>
2013-07-30 13:20:49 +0100

    add a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:base_title) { "Mini Twitter app" }
 
   	describe "Home page" do
 
@@ -11,7 +12,12 @@ describe "Static pages" do
 	    
 	    it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title('Mini Twitter app | Home')
+			expect(page).to have_title(" #{ base_title }")
+	    end
+
+	    it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title(" | Home")
 	    end
   	end
    	describe "Help page" do
@@ -23,7 +29,7 @@ describe "Static pages" do
 
 	    it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title('Mini Twitter app | Help')
+			expect(page).to have_title(" #{ base_title } | Help")
 	    end
   	end
     describe "About page" do
@@ -35,7 +41,19 @@ describe "Static pages" do
 	    
 	    it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			expect(page).to have_title('Mini Twitter app | About Us')
+			expect(page).to have_title(" #{ base_title } | About Us")
+	    end
+  	end
+    describe "Contact page" do
+
+	    it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+	    end
+	    
+	    it "should have the title 'About Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title(" #{ base_title } | Contact")
 	    end
   	end
 end
