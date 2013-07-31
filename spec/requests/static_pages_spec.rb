8af3cbee98f3eceea410b0390c5d1d1e3135129e
nolidais <nolidais@gmail.com>
2013-07-31 15:21:30 +0100

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   	describe "Home page" do
 		before { visit root_path }
 	    it { should have_content('Mini Twitter') }
-	    it { should  have_title full_title '' }
+	    it { should have_title full_title '' }
 	    it { should_not have_title(" | Home") }
   	end
 
@@ -23,7 +23,22 @@ describe "Static pages" do
   	end
     describe "Contact page" do
     	before { visit contact_path }
+    	it { should have_selector('h1', text: 'Contact') }
 	    it { should have_content('Contact') }
 	    it { should have_title full_title 'Contact' }
   	end
+  	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign up'))
+		click_link "Mini Twitter"
+		expect(page).to have_title(full_title(''))
+	end
 end
